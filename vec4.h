48e2b01509c5167e229a3bf1d7a936a36ba0eed0
רבקה בר-סף <rebeccoosh@gmail.com>
2016-12-11 18:34:26 +0200

    intial commit

---
+++ vec4.h
@@ -0,0 +1,19 @@
+#pragma once
+class vec4
+{
+public:
+	vec4();
+	vec4(double _x, double _y, double _z, double _p);
+	~vec4();
+	unsigned int hash();
+	double x;
+	double y;
+	double z;
+	double p;
+	double& operator[](int indx);
+};
+
+bool operator==(vec4 l, vec4 r);
+vec4 operator+(vec4 lhs, vec4 rhs);
+vec4 operator-(vec4 lhs, vec4 rhs);
+vec4 operator/(vec4 lhs, double x);
