e04ac5118f21269efd757d9c4721e3630550295c
giltsuker <giltsuker@yahoo.com>
2016-12-23 16:10:50 +0200

    Add files via upload

--- CGWorkView.h
+++ CGWorkView.h
@@ -57,9 +57,10 @@ private:
 	
 
 	// our functions
-	void DrawLine(int *z_arr, COLORREF *arr, vec4 &p1, vec4 &p2, COLORREF color, std::unordered_map<int, std::vector<int>>* x_y = NULL);
-	void DrawBoundBox(int *z_arr, COLORREF *arr, model &m, mat4 cur_transform, COLORREF color);
-	void ScanConversion(int *z_arr, COLORREF *arr, polygon &p, mat4 cur_transform, COLORREF color);
+	void DrawLine(double *z_arr, COLORREF *arr, vec4 &p1, vec4 &p2, COLORREF color, std::unordered_map<int, std::vector<int>>* x_y = NULL);
+	void DrawBoundBox(double *z_arr, COLORREF *arr, model &m, mat4 cur_transform, COLORREF color);
+	void ScanConversion(double *z_arr, COLORREF *arr, polygon &p, mat4 cur_transform, COLORREF color);
+	COLORREF ApplyLight(COLORREF in_color, vec4 normal, vec4 pos);
 	COLORREF m_color_wireframe;
 	COLORREF m_background_color;
 	COLORREF m_boundbox_color;
@@ -116,7 +117,7 @@ protected:
 	double m_AspectRatio;		// hold the fixed Aspect Ration
 	double m_mouse_sensetivity;
 	COLORREF *m_screen;
-	int *z_buffer;
+	double *z_buffer;
 	HBITMAP m_map;
 
 // Generated message map functions
@@ -155,6 +156,7 @@ protected:
 	afx_msg void OnActionToggleView();
 	afx_msg void OnUpdateActionToggleView(CCmdUI* pCmdUI);
 	afx_msg void OnActionResetView();
+	afx_msg void OnActionClearAll();
 	afx_msg void OnOptionMouseSensetivity();
 	afx_msg void OnOptionPrespectiveControl();
 	afx_msg void OnOptionOthers();
